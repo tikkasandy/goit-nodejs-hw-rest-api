2a24e6cfa9d98028d4dfa55885fc37ced4537915
tikkasandy <82177700+tikkasandy@users.noreply.github.com>
2022-04-14 21:15:09 +0300

    Add pagination

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -3,7 +3,10 @@ const { HTTP_STATUS_CODE } = require('../../libs/constants')
 
 const listContacts = async (req, res) => {
     const { _id } = req.user
-    const contacts = await Contact.find({ owner: _id }).populate("owner", "_id email")
+    const { page = 1, limit = 10 } = req.query
+    const skip = (page - 1) * limit
+
+    const contacts = await Contact.find({ owner: _id }, "", { skip, limit: Number(limit) }).populate("owner", "_id email")
 
     res.json({
         status: 'success',
